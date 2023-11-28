7bcaa6fbd9855a28da24ccbfa9657240e44e0aa4
edwinMV <78721227+edwinMV@users.noreply.github.com>
2023-11-28 09:12:33 -0700

    library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksModalComponent } from './books-modal/books-modal.component';
+import { BooksPageComponent } from './books-page/books-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  { path: 'books', component: BooksPageComponent },
+  { path: 'books-modal', component: BooksModalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksModalComponent } from './books-modal/books-modal.component';
+import { BooksPageComponent } from './books-page/books-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksModalComponent,
+    BooksPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
